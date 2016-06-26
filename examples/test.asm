f917f73cc3134d2932899fb7954096eea819e12d
David Beckley <david@realstax.com>
2016-06-25 22:36:18 -0700

    Flesh out example

--- examples/test.asm
+++ examples/test.asm
@@ -5,12 +5,43 @@ bss {
 }
 
 raw {
+  factorial:
+    ENT 0x0
+    REL 0x0
+    LI
+    PSH
+    IMM 0x0
+    EQ
+    BZ 0x1d
+    IMM 0x1
+    RET
+    REL 0x0
+    LI
+    PSH
+    IMM 0x1
+    SUB
+    PUSHARG
+    JSR factorial
+    ADJ 0x1
+    PSH
+    REL 0x0
+    LI
+    MUL
+    RET
+    RET
+
   main:
     ENT 0x0
     IMM message
     PUSHARG
     INT 0x2      ; printstring()
     ADJ 0x1
+    IMM 0x7
+    PUSHARG
+    JSR factorial
+    ADJ 0x1
+    PSH
+    INT 0x1      ; printint()
     IMM 0x0
     RET
     RET
