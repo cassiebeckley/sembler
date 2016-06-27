2d28e04b43d8e920e81aca00918a005ada905d22
David Beckley <beckl.ds@gmail.com>
2016-06-27 00:07:03 -0700

    Add argument parser and some options

--- src/bin/sembler.rs
+++ src/bin/sembler.rs
@@ -1,22 +1,41 @@
 extern crate sembler;
+extern crate clap;
 extern crate rustc_serialize as serialize;
 
-use std::env;
 use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
 
-use sembler::parser;
-use sembler::assemble;
+use sembler::{parser, assemble};
+
+use clap::{Arg, App};
 
 use serialize::json::ToJson;
 
 fn main() {
-    if env::args().count() < 2 {
-        panic!("Usage: sembler <source_file>");
-    }
+    let matches = App::new("Sembler")
+    .version("0.1.0")
+    .author("David Beckley <beckl.ds@gmail.com>")
+    .about("An assembler for the Stockfighter Virtual Machine")
+    .arg(Arg::with_name("entry-point")
+        .short("e")
+        .long("entry-point")
+        .value_name("SYMBOL")
+        .help("Sets the entry point to SYMBOL")
+        .takes_value(true))
+    .arg(Arg::with_name("output")
+        .short("o")
+        .long("output")
+        .value_name("FILE")
+        .help("Write output to FILE")
+        .takes_value(true))
+    .arg(Arg::with_name("INPUT")
+        .help("Input file")
+        .required(true)
+        .index(1))
+    .get_matches();
 
-    let file = env::args().nth(1).unwrap();
+    let file = matches.value_of("INPUT").unwrap();
     let path = Path::new(&file);
 
     let mut file = File::open(&path).unwrap();
@@ -27,8 +46,7 @@ fn main() {
         bytes
     };
 
-    // TODO: take this as arg
-    let entry_point = "main";
+    let entry_point = matches.value_of("entry-point").unwrap_or("main");
 
     let ast = match parser::parse_svm(&source) {
         Ok(ast) => ast,
@@ -36,5 +54,11 @@ fn main() {
     };
     let blob = assemble::assemble(&ast, entry_point);
 
-    println!("{}", blob.to_json().pretty());
+    match matches.value_of("output") {
+        Some(filename) => {
+            let mut f = File::create(filename).unwrap();
+            write!(f, "{}", blob.to_json().pretty()).unwrap();
+        },
+        None => println!("{}", blob.to_json().pretty())
+    }
 }
